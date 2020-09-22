e026ef6f45e4bfdd2d8a299389dab53318073543
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-09-22 09:36:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function tri(row, column){
   x = factorial(row)/(factorial(row-column)*factorial(column));
   return x;
 }
-for (let 1=0; i<row+1; i++){
-  alert(tri(row, i))
+for (let i=0; i<row+1; i++){
+  alert(tri(row, i));
 }
 function factorial(n) {
   return n ? n * factorial(n - 1) : 1;
