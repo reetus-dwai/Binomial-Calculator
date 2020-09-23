144357ad976bec5f3fa11a4cc018566808c0bcdc
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-09-22 21:47:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let row = 5;
+let row = 30;
 
 function tri(row, column) {
   let x;
-	x = (factorial(row) / (factorial(row - column) * factorial(column)));
+	x = Math.round((factorial(row) / (factorial(row - column) * factorial(column))));
   return x;
 }
 let h = [];
 for (let i=0; i<row+1; i++){
 	h.push(tri(row, i));
-	document.getElementById('ans').innerHTML = h + ' ';
+	document.getElementById('ans').innerHTML = h;
 }
 function factorial(n) {
   return n ? n * factorial(n - 1) : 1;
@@ -16,4 +16,7 @@ function factorial(n) {
 function calc() {
 	let exponent = document.getElementById('exponent').value;
   let expression = document.getElementById('expression').value;
-  document.getElementById('ans').innerHTML = h + '・'}
+  document.getElementById('ans').innerHTML = h[0]}
+
+
+ /*・*/
