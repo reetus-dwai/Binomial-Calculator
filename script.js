d511cbaefded69f23f7c730e6ab2b1d4e1329b44
reetus-dwai <67768149+reetus-dwai@users.noreply.github.com>
2020-09-22 18:50:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 let row = 5;
 
 function tri(row, column) {
-  let x = [];
-  x = factorial(row) / (factorial(row - column) * factorial(column));
+  let x;
+	x = (factorial(row) / (factorial(row - column) * factorial(column)));
   return x;
 }
-/* for (let i=0; i<row+1; i++){
-  alert(tri(row, i));
-} */
+let h = [];
+for (let i=0; i<row+1; i++){
+	h.push(tri(row, i));
+	document.getElementById('ans').innerHTML = h + ' ';
+}
 function factorial(n) {
   return n ? n * factorial(n - 1) : 1;
 }
-
-function answer() {
-	let a=0;
-	let h=[];
-	for (let i=0; i<5; i++){
-		h.push();
-	}
-	return h;
-}
-
 function calc() {
-  let exponent = document.getElementById('exponent').value;
+	let exponent = document.getElementById('exponent').value;
   let expression = document.getElementById('expression').value;
-  document.getElementById('ans').innerHTML = 'You answer is: ' + expression;
-}
+  document.getElementById('ans').innerHTML = 'You answer is: ' + expression;}
